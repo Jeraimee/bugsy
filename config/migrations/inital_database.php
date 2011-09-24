fc8642376f47930c5f1f6df0cd99295e15c8c222
Jeraimee <a.sleep@asleep.net>
2011-09-24 10:39:12 -0400

    Added inital DB migration

---
+++ config/migrations/inital_database.php
@@ -0,0 +1,120 @@
+<?php
+class M4e7deaa7f8ec495c8db0c14cea73fbca extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'comments' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'issue_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'comment' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => array('id', 'issue_id', 'user_id'), 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+				'issues' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'project_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'subject' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'description' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'url' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'status' => array('type' => 'string', 'null' => false, 'default' => 'New', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'priority' => array('type' => 'string', 'null' => false, 'default' => 'P3', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => array('id', 'project_id', 'user_id'), 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+				'projects' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'description' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'public_view' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+					'public_add' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+					'issue_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+				'users' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+					'username' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'index', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'email_address' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'confirmed' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'username' => array('column' => array('username', 'email_address'), 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'comments', 'issues', 'projects', 'users'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction)
+	{
+  	if ($direction === 'up') {
+  		if (!class_exists('Security')) {
+  			App::import('Core', 'Security');
+  		}
+
+  		$User = $this->generateModel('User');
+  		$user = array('User' => array('username'      => 'admin',
+                                    'password'      => Security::hash('password', null, true),
+                                    'email_address' => 'admin@example.com'));
+  		$User->save($user);
+  	}
+		return true;
+	}
+}
+?>
---
+++ config/migrations/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'inital_database' => 'M4e7deaa7f8ec495c8db0c14cea73fbca'),
+);
+?>
